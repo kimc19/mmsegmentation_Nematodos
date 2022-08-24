5a34bf082b311c4f6bbc567ae897c76c6cc381a9
Kimberly <kimc19@outlook.com>
2022-08-24 17:12:24 -0600

    Script editable para crear configs de modelos

---
+++ config_utils/create_config_pspnet_nematodos.py
@@ -0,0 +1,106 @@
+from mmcv import Config
+from mmseg.apis import set_random_seed
+from mmseg.utils import get_device
+
+data_root = '../../../mmsegmentation/data/nematodos'
+img_dir = 'images'
+ann_dir = 'annotations'
+
+cfg = Config.fromfile('../../../mmsegmentation/configs/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes.py')
+
+# Since we use only one GPU, BN is used instead of SyncBN
+cfg.norm_cfg = dict(type='BN', requires_grad=True)
+cfg.model.backbone.norm_cfg = cfg.norm_cfg
+cfg.model.decode_head.norm_cfg = cfg.norm_cfg
+cfg.model.auxiliary_head.norm_cfg = cfg.norm_cfg
+# modify num classes of the model in decode/auxiliary head
+cfg.model.decode_head.num_classes = 2
+cfg.model.auxiliary_head.num_classes = 2
+
+# Modify dataset type and path
+cfg.dataset_type = 'NematodosDataset'
+cfg.data_root = data_root
+
+cfg.data.samples_per_gpu = 8
+cfg.data.workers_per_gpu = 2
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (512, 512)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='Resize', img_scale=(1024, 768), ratio_range=(0.5, 2.0)),
+    dict(type='RandomRotate', prob=0.75, degree=180),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 768),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+cfg.data.train.type = cfg.dataset_type
+cfg.data.train.data_root = cfg.data_root
+cfg.data.train.img_dir = img_dir
+cfg.data.train.ann_dir = ann_dir
+cfg.data.train.pipeline = cfg.train_pipeline
+cfg.data.train.split = 'splits/train.txt'
+
+cfg.data.val.type = cfg.dataset_type
+cfg.data.val.data_root = cfg.data_root
+cfg.data.val.img_dir = img_dir
+cfg.data.val.ann_dir = ann_dir
+cfg.data.val.pipeline = cfg.test_pipeline
+cfg.data.val.split = 'splits/val.txt'
+
+cfg.data.test.type = cfg.dataset_type
+cfg.data.test.data_root = cfg.data_root
+cfg.data.test.img_dir = img_dir
+cfg.data.test.ann_dir = ann_dir
+cfg.data.test.pipeline = cfg.test_pipeline
+cfg.data.test.split = 'splits/test.txt'
+
+cfg.work_dir = './work_dirs/prueba'
+
+cfg.runner.max_iters = 200
+cfg.evaluation.interval = 200
+cfg.checkpoint_config.interval = 50
+cfg.checkpoint_config.max_keep_ckpts = 2
+
+# Set seed to facitate reproducing the result
+cfg.seed = 0
+set_random_seed(0, deterministic=False)
+cfg.gpu_ids = range(1)
+cfg.device = get_device()
+
+# Set wand hook
+cfg.log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='MMSegWandbHook',
+             init_kwargs={'project': 'Prueba_Nematodos_20'},
+             log_checkpoint=True,
+             #log_checkpoint_metadata=True,
+             num_eval_images=4)
+        ])
+
+# Let's have a look at the final config used for training
+print(f'Config:\n{cfg.pretty_text}')
+cfg.dump("../pspnet_nematodos.py")
