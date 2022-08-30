c34a721d3ad5905c3e04acf4b45ceab7de88cb03
Kimberly <kimc19@outlook.com>
2022-08-30 15:11:51 -0600

    Correciòn de error para modificar num_classes

--- config_utils/create_config_stdc1.py
+++ config_utils/create_config_stdc1.py
@@ -20,15 +20,56 @@ ann_dir = 'annotations'
 
 cfg = Config.fromfile('../configs/stdc/stdc1_512x1024_80k_cityscapes.py')
 
-# Since we use only one GPU, BN is used instead of SyncBN
-cfg.norm_cfg = dict(type='BN', requires_grad=True)
-cfg.model.backbone.norm_cfg = cfg.norm_cfg
-cfg.model.decode_head.norm_cfg = cfg.norm_cfg
-cfg.model.auxiliary_head.norm_cfg = cfg.norm_cfg
-
 # Modify num classes of the model in decode/auxiliary head
-cfg.model.decode_head.num_classes = 2
-cfg.model.auxiliary_head.num_classes = 2
+norm_cfg = dict(type='BN', requires_grad=True)
+cfg.model.decode_head.num_classes=2
+cfg.model.auxiliary_head=[
+        dict(
+            type='FCNHead',
+            in_channels=128,
+            channels=64,
+            num_convs=1,
+            num_classes=2,
+            in_index=2,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=10000),
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='FCNHead',
+            in_channels=128,
+            channels=64,
+            num_convs=1,
+            num_classes=2,
+            in_index=1,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=10000),
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='STDCHead',
+            in_channels=256,
+            channels=64,
+            num_convs=1,
+            num_classes=2,
+            boundary_threshold=0.1,
+            in_index=0,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=True,
+            loss_decode=[
+                dict(
+                    type='CrossEntropyLoss',
+                    loss_name='loss_ce',
+                    use_sigmoid=True,
+                    loss_weight=1.0),
+                dict(type='DiceLoss', loss_name='loss_dice', loss_weight=1.0)
+            ]),
+    ]
 
 # Modify dataset type and path
 cfg.dataset_type = 'NematodosDataset'
