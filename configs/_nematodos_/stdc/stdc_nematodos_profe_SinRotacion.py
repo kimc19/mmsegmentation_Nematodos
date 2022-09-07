81983867a1b9376ce2c06d749fc0ae81bf1f713b
Kimberly <kimc19@outlook.com>
2022-09-07 14:57:52 -0600

    Config profe sin rotacion

--- configs/_nematodos_/stdc/stdc_nematodos_profe_SinRotacion.py
+++ configs/_nematodos_/stdc/stdc_nematodos_profe_SinRotacion.py
@@ -203,11 +203,11 @@ log_config = dict(
             init_kwargs=dict(
                 entity='kimc19',
                 project='STDC_Prueba',
-                name='profe',
-                id='profe',
+                name='profe_SinRot',
+                id='profe_SinRot',
                 resume='allow',
                 notes=
-                'Prueba entrenamiento modelo stdc base, aumentado profe, 5k iteraciones, batch=8'
+                'Prueba entrenamiento modelo stdc base, aumentado profe sin rotación, 5k iteraciones, batch=8'
             ),
             log_checkpoint=True,
             log_checkpoint_metadata=True,
@@ -232,7 +232,7 @@ runner = dict(type='IterBasedRunner', max_iters=4000)
 checkpoint_config = dict(by_epoch=False, interval=2000, max_keep_ckpts=2)
 evaluation = dict(
     interval=2000, metric=['mIoU', 'mDice', 'mFscore'], pre_eval=True)
-work_dir = '../work_dirs/stdc_profe'
+work_dir = '../work_dirs/stdc_profe_SinRot'
 seed = 0
 gpu_ids = range(0, 1)
 device = 'cuda'
