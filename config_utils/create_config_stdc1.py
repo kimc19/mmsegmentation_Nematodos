4bfd5be748213cfeca3dee9b6e70e02fcc52436b
Kimberly <kimc19@outlook.com>
2022-09-12 14:43:12 -0600

    Se quita reduce_zero_label=True

--- config_utils/create_config_stdc1.py
+++ config_utils/create_config_stdc1.py
@@ -77,7 +77,7 @@ cfg.data_root = data_root
 
 cfg.data.samples_per_gpu = 10
 cfg.data.workers_per_gpu = 2 #Wand support
-cfg.val_dataloader=dict(samples_per_gpu=1, workers_per_gpu=2, shuffle=False)
+cfg.data.val_dataloader=dict(samples_per_gpu=1, workers_per_gpu=2, shuffle=False)
 
 cfg.img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
