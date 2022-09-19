ceaab3c49a1f466fd3d18d87eb023b5799a11b11
Kimberly <kimc19@outlook.com>
2022-09-19 15:56:38 -0600

    Cambio nombre de corrida para wandb

--- configs/_nematodos_/pointrend/pointrend_pretrain.py
+++ configs/_nematodos_/pointrend/pointrend_pretrain.py
@@ -177,8 +177,8 @@ log_config = dict(
             init_kwargs=dict(
                 entity='kimc19',
                 project='PointRend_Nematodos',
-                name='pointrend_Pretrain',
-                id='pointrend_Pretrain',
+                name='pointrend__Pretrain',
+                id='pointrend__Pretrain',
                 resume='allow',
                 notes=
                 'Entrenamiento modelo pointrend pretrain, lr=0.01, m=0.9, A1, 80k iteraciones, batch=2'
