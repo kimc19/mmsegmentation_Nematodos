b511ebc301fd83d2796294b860ab066d0ea209c7
Kimberly <kimc19@outlook.com>
2022-09-20 17:12:13 -0600

    Archivo que contiene los diferentes aumentos de datos que se aplican

---
+++ config_utils/aumentados.py
@@ -0,0 +1,65 @@
+#Aumentado 1
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (768, 1024)
+cfg.train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(1024, 768), ratio_range=(0.5, 1.5)),
+    dict(type='RandomRotate', prob=0.75, degree=30),
+    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.25),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **cfg.img_norm_cfg),
+    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 768),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+#Aumentado 2
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (768, 1024)
+cfg.train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(1024, 768), ratio_range=(0.5, 1.5)),
+    dict(type='RandomRotate', prob=0.75, degree=30),
+    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.25),
+    dict(type='Normalize', **cfg.img_norm_cfg),
+    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 768),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomRotate', prob=0.75, degree=30),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
