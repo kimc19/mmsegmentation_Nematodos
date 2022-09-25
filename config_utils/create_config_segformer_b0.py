6e7c6311ae3c7e6eb42abfe7a0041e6637f68e09
Kimberly <kimc19@outlook.com>
2022-09-24 19:29:36 -0600

    Se agrega flip del test pipeline

--- config_utils/create_config_segformer_b0.py
+++ config_utils/create_config_segformer_b0.py
@@ -56,15 +56,15 @@ cfg.test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         img_scale=(1024, 768),
+        flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
-            dict(type='RandomRotate', prob=0.75, degree=30),
+            dict(type='RandomFlip'),
             dict(type='Normalize', **cfg.img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
-
 
 cfg.data.train.type = cfg.dataset_type
 cfg.data.train.data_root = cfg.data_root
