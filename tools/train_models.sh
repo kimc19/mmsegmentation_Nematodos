57b7071e7ae5f47b6a089adc8e1fcf19a0f2b423
Kimberly <kimc19@outlook.com>
2022-09-27 13:10:54 -0600

    Entrenamiento de segformer con 80k iteraciones

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/stdc/stdc2_A2.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_A2.py
-python train.py ../configs/_nematodos_/segformer/segformerb0_A2.py
+python train.py ../configs/_nematodos_/segformer/segformerb0_base_80k.py
+python train.py ../configs/_nematodos_/segformer/segformerb0_pretrain_80k.py
+python train.py ../configs/_nematodos_/segformer/segformerb0_A2_80k.py
