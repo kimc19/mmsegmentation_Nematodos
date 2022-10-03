69c987186b91133d9ba2572718bd3d31607c0ab1
Kimberly <kimc19@outlook.com>
2022-10-02 20:58:36 -0600

    Pruebas de estabilidad

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/segformer/segformerb0_base_80k.py
-python train.py ../configs/_nematodos_/segformer/segformerb0_pretrain_80k.py
-python train.py ../configs/_nematodos_/segformer/segformerb0_A2_80k.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain01.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain02.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain03.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain04.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain05.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain06.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain07.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain08.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain09.py
+python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain10.py
