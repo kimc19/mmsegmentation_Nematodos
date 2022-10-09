b84b61a47d51e1039c1137598d215e330b6f15b7
Kimberly <kimc19@outlook.com>
2022-10-08 20:21:53 -0600

    Bash para entrenamiento de bisenetv2

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain01.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain02.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain03.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain04.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain05.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain06.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain07.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain08.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain09.py
-python train.py ../configs/_nematodos_/estabilidad/segformerb0_pretrain10.py
+python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_base.py
+python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_pretrain.py
+python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_A2.py
