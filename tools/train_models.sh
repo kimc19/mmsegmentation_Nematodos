2cef0e3719d1f4e35e6841a96e65906dcabff321
Kimberly <kimc19@outlook.com>
2022-10-11 01:48:02 -0600

    Secuencia de modelos con cambio de hiperparametros, cambio en ruta

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/pointrend_P2048.py
-python train.py ../configs/_nematodos_/pointrend_P768.py
-python train.py ../configs/_nematodos_/pointrend_P49.py
-python train.py ../configs/_nematodos_/pointrend_P196.py
-python train.py ../configs/_nematodos_/pointrend_P784.py
-python train.py ../configs/_nematodos_/pointrend_P3136.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P2048.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P768.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P49.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P196.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P784.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P3136.py
