b037f6134cf133a93f40c6ecc6fb52af68b2e5ba
Kimberly <kimc19@outlook.com>
2022-10-11 01:45:19 -0600

    Secuencia de modelos con cambio de hiperparametros

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_base.py
-python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_pretrain.py
-python train.py ../configs/_nematodos_/bisenetv2/bisenetv2_A2.py
+python train.py ../configs/_nematodos_/pointrend_P2048.py
+python train.py ../configs/_nematodos_/pointrend_P768.py
+python train.py ../configs/_nematodos_/pointrend_P49.py
+python train.py ../configs/_nematodos_/pointrend_P196.py
+python train.py ../configs/_nematodos_/pointrend_P784.py
+python train.py ../configs/_nematodos_/pointrend_P3136.py
