e63e232a975837607526b4a3f0fe661d9245ac2e
Kimberly <kimc19@outlook.com>
2022-10-12 17:30:21 -0600

    Prueba de estabilidad pointrend y prueba 3136 puntos

--- tools/train_models.sh
+++ tools/train_models.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-python train.py ../configs/_nematodos_/pointrend/pointrend_P2048.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_P768.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_P49.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_P196.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_P784.py
-python train.py ../configs/_nematodos_/pointrend/pointrend_P3136.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_1.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_2.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_3.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_4.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_5.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_6.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_7.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_8.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_9.py
+python train.py ../configs/_nematodos_/estabilidad/pointrend_10.py
+python train.py ../configs/_nematodos_/pointrend/pointrend_P3136_resume.py
