413f8454db74a326ec008dd1cca039d2f682c82f
Kimberly <kimc19@outlook.com>
2022-11-08 23:00:30 -0600

    bash para ejecutar get_flops

---
+++ tools/flops.sh
@@ -0,0 +1,13 @@
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P49.py > ../../../logs/flops_pointrend_P49.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P196.py > ../../../logs/flops_pointrend_P196.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P768.py > ../../../logs/flops_pointrend_P768.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P784.py > ../../../logs/flops_pointrend_P784.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P1024.py > ../../../logs/flops_pointrend_P1024.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P2048.py > ../../../logs/flops_pointrend_P2048.txt 2>&1
+
+python get_flops.py ../configs/_nematodos_/pointrend/pointrend_P3136.py > ../../../logs/flops_pointrend_P3136.txt 2>&1
